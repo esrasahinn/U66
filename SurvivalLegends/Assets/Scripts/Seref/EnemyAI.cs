6cac61ace766d8d98e18d362ea48df03799abfec
Serclarus <serefsakaci@hotmail.fr>
2023-06-12 21:42:33 +0300

    EnemyAI

--- SurvivalLegends/Assets/Scripts/Seref/EnemyAI.cs
+++ SurvivalLegends/Assets/Scripts/Seref/EnemyAI.cs
@@ -6,18 +6,24 @@ using UnityEngine.AI;
 public class NewBehaviourScript : MonoBehaviour
 
 {
+    private Animator animator;
     public NavMeshAgent enemy;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
+    public GameObject projectile;
+    public float projectileSpeed = 10.0f;
     public float attackCooldown;
     public bool alreadyAttacked;
-    public float attackRange;
+    public float attackRange = 10.0f;
     public bool inAttackRange;
+
+    
 
     void Awake()
     {
         player = GameObject.Find("Player").transform;
         enemy = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -36,16 +42,22 @@ public class NewBehaviourScript : MonoBehaviour
     void ChasePlayer()
     {
         enemy.SetDestination(player.position);
+        animator.SetFloat("Speed", 1);
     }
 
     void AttackPlayer()
     {
         enemy.SetDestination(transform.position);
+        animator.SetFloat("Speed", 0);
+        animator.SetBool("AttackRange", true);
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
+            Rigidbody projectileRb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            projectileRb.AddForce(transform.forward * projectileSpeed, ForceMode.Impulse);
             alreadyAttacked = true;
+            animator.SetBool("InAttackCD", true);
             Invoke(nameof(ResetAttack), attackCooldown);
         }
     }
@@ -53,5 +65,6 @@ public class NewBehaviourScript : MonoBehaviour
     void ResetAttack()
     {
         alreadyAttacked = false;
+        animator.SetBool("InAttackCD", false);
     }
 }
