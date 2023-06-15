00eebba0b4fae74e96c483462818ed64693a3b37
Furkan <furkanpatlar@hotmail.com>
2023-06-15 08:42:46 +0300

    Turkce harflerin duzeltilmesi

--- SurvivalLegends/Assets/MenzileGirenDusmanaAtesVeDonme.cs
+++ SurvivalLegends/Assets/MenzileGirenDusmanaAtesVeDonme.cs
@@ -1,90 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MenzileGirenDusmanaAtesVeDonme : MonoBehaviour
 {
-    public float menzilMesafesi = 10f;
-    public float donmeHizi = 5f;
-    public float atesHizi = 10f;
-    public float ateþEtmeMesafesi = 5f;
-    public float hedefeGitmeHizi = 5f;
-    public float donmeBaslamaMesafesi = 3f;
-    public float donmeBitisMesafesi = 7f;
-    public float mermiOmru = 5f;
-    public GameObject mermiPrefab;
-    public Transform ateþNoktasý;
-    public Transform ozelYetenekAteþNoktasý; // Özel yetenek ateþ noktasý transformu
-    public float ozelYetenekAraligi = 10f;
-    public float ozelYetenekAtesHizi = 5f;
-    public GameObject ozelYetenekPrefab; // Özel yetenek prefabý
+    public float menzilMesafesi = 10f; // Ateþin menzil mesafesi
+    public float donmeHizi = 5f; // Dönme hýzý
+    public float atesHizi = 10f; // Ateþ hýzý (saniyede ateþ sayýsý)
+    public float atesEtmeMesafesi = 5f; // Ateþ etme mesafesi
+    public float hedefeGitmeHizi = 5f; // Merminin hedefe gitme hýzý
+    public float donmeBaslamaMesafesi = 3f; // Dönme mesafesi baþlangýcý
+    public float donmeBitisMesafesi = 7f; // Dönme mesafesi bitiþi
+    public float mermiOmru = 5f; // Mermi ömrü (saniye)
+    public GameObject mermiPrefab; // Merminin prefabý
+    public Transform atesNoktasi; // Ateþin baþlayacaðý nokta
 
-    private GameObject hedef;
-    private bool dusmanaDonuyor = false;
-    private bool menzilde = false;
-    private float sonrakiAteþZamaný = 0f;
-    private bool ozelYetenekAktif = false;
-    private float sonrakiOzelYetenekZamani = 0f;
-
-    [SerializeField] private Button ozelYetenekButonu;
-
-    private void Start()
-    {
-        // Özel yetenek butonuna týklama olayýný dinle
-        ozelYetenekButonu.onClick.AddListener(AktifEtOzelYetenek);
-    }
+    private GameObject hedef; // Menzile giren düþman hedefi
+    private bool dusmanaDonuyor = false; // Düþmana dönme durumu
+    private bool menzilde = false; // Menzile girdi durumu
+    private float sonrakiAtesZamani = 0f; // Sonraki ateþ zamanýný takip etmek için kullanýlýr
 
     private void Update()
     {
+        // Hedef yoksa veya hedef menzil dýþýndaysa, yeni bir hedef seç
         if (hedef == null || Vector3.Distance(transform.position, hedef.transform.position) > menzilMesafesi)
         {
             HedefSec();
-            dusmanaDonuyor = false;
-            menzilde = false;
+            dusmanaDonuyor = false; // Düþmana dönme durumunu sýfýrla
+            menzilde = false; // Menzile girdi durumunu sýfýrla
         }
 
+        // Hedef seçildiyse ve düþmana dönme mesafesine geldiyse düþmana doðru dön
         if (hedef != null && Vector3.Distance(transform.position, hedef.transform.position) <= donmeBitisMesafesi)
         {
             if (!menzilde && Vector3.Distance(transform.position, hedef.transform.position) <= donmeBaslamaMesafesi)
             {
-                dusmanaDonuyor = true;
-                menzilde = true;
+                dusmanaDonuyor = true; // Düþmana dönme durumunu etkinleþtir
+                menzilde = true; // Menzile girdi durumunu etkinleþtir
             }
             DusmanaDon();
         }
         else
         {
-            dusmanaDonuyor = false;
-            menzilde = false;
+            dusmanaDonuyor = false; // Dönme durumunu sýfýrla
+            menzilde = false; // Menzilden çýktý durumunu sýfýrla
         }
 
-        if (hedef != null && Vector3.Distance(transform.position, hedef.transform.position) <= ateþEtmeMesafesi && Time.time >= sonrakiAteþZamaný)
+        // Hedef seçildiyse, ateþ etme mesafesine geldiyse ve ateþ zamaný geldiyse ateþ et
+        if (hedef != null && Vector3.Distance(transform.position, hedef.transform.position) <= atesEtmeMesafesi && Time.time >= sonrakiAtesZamani)
         {
-            if (ozelYetenekAktif)
-            {
-                AtesEt(ozelYetenekAteþNoktasý); // Özel yetenek ateþ noktasýný kullanarak ateþ et
-                sonrakiAteþZamaný = Time.time + 1f / ozelYetenekAtesHizi;
-            }
-            else
-            {
-                AtesEt(ateþNoktasý); // Normal ateþ noktasýný kullanarak ateþ et
-                sonrakiAteþZamaný = Time.time + 1f / atesHizi;
-            }
+            AtesEt(); // Ateþ etme fonksiyonunu çaðýrýr
+            dusmanaDonuyor = true; // Düþmana dönme durumunu etkinleþtir
 
-            dusmanaDonuyor = true;
-        }
-
-        if (Time.time >= sonrakiOzelYetenekZamani)
-        {
-            ozelYetenekAktif = false;
+            sonrakiAtesZamani = Time.time + 1f / atesHizi; // Sonraki ateþ zamanýný günceller
         }
     }
 
     void HedefSec()
     {
+        // Tüm düþmanlarý al
         GameObject[] dusmanlar = GameObject.FindGameObjectsWithTag("Dusman");
 
+        // En yakýn düþmaný bul ve hedef olarak seç
         float enYakinMesafe = Mathf.Infinity;
         GameObject enYakinDusman = null;
 
@@ -99,37 +76,25 @@ public class MenzileGirenDusmanaAtesVeDonme : MonoBehaviour
             }
         }
 
+        // Hedefi güncelle
         hedef = enYakinDusman;
     }
 
     void DusmanaDon()
     {
         Vector3 hedefYonu = hedef.transform.position - transform.position;
-        hedefYonu.y = 0f;
+        hedefYonu.y = 0f; // Y ekseni üzerinde dönmemek için sýfýrla
         Quaternion hedefRotasyonu = Quaternion.LookRotation(hedefYonu);
         transform.rotation = Quaternion.Slerp(transform.rotation, hedefRotasyonu, donmeHizi * Time.deltaTime);
     }
 
-    void AtesEt(Transform atesNoktasi)
+    void AtesEt()
     {
         GameObject mermi = Instantiate(mermiPrefab, atesNoktasi.position, atesNoktasi.rotation);
-        Mermi mermiScript = mermi.GetComponent<Mermi>();
-        mermiScript.HedefBelirle(hedef.transform);
-       // mermiScript.HizAyarla(hedefeGitmeHizi);
+        Mermi mermiScript = mermi.GetComponent<Mermi>(); // Mermi scriptini al
+        mermiScript.HedefBelirle(hedef.transform); // Mermi scriptindeki HedefBelirle fonksiyonunu çaðýr
+        //mermiScript.HizAyarla(hedefeGitmeHizi); // Mermi hýzýný ayarla
 
-        Destroy(mermi, mermiOmru);
-    }
-
-    public void AktifEtOzelYetenek()
-    {
-
-        if (!ozelYetenekAktif)
-        {
-
-            ozelYetenekAktif = true;
-            sonrakiOzelYetenekZamani = Time.time + ozelYetenekAraligi;
-
-            Instantiate(ozelYetenekPrefab, transform.position, transform.rotation);
-        }
+        Destroy(mermi, mermiOmru); // Mermi objesini belirli bir süre sonra yok et
     }
 }
