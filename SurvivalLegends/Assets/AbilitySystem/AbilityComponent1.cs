82d1c9c4ef5beeadc633d576249f2e4ac0960082
Furkan <furkanpatlar@hotmail.com>
2023-06-22 20:44:47 +0300

    Revert "Revert "yetenekCooldown""
    This reverts commit b3864c6dd6c25f0307cfc39de8243406109680ab.

---
+++ SurvivalLegends/Assets/AbilitySystem/AbilityComponent1.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AbilityComponent1 : MonoBehaviour, IPurchaseListener
+{
+    [SerializeField] Ability[] InitialAbilites;
+
+    public delegate void OnNewAbilityAdded(Ability newAbility);
+    public delegate void OnStaminaChange(float newAmount, float maxAmount);
+
+    private List<Ability> abilities = new List<Ability>();
+
+    public event OnNewAbilityAdded onNewAbilityAdded;
+    public event OnStaminaChange onStaminaChange;
+
+    [SerializeField] float stamina = 200f;
+    [SerializeField] float maxStamina = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //foreach(Ability ability in InitialAbilites)
+       //{
+       //    GiveAbility(ability);
+       //}
+
+    }
+   //void GiveAbility(Ability ability)
+   //{
+   //    Ability newAbility = Instantiate(ability);
+   //    newAbility.InitAbility(this);
+   //    abilities.Add(newAbility);
+   //    onNewAbilityAdded?.Invoke(newAbility);
+   //
+   //}
+
+
+    public void ActiveAbility(Ability abilityToActive)
+    {
+        if(abilities.Contains(abilityToActive))
+        {
+            abilityToActive.ActivateAbility();
+        }
+    }
+
+    float GetStamina()
+    {
+        return stamina;
+    }
+
+    public bool TryConsumeStamina(float staminaToConsume)
+    {
+        if (stamina <= staminaToConsume) return false;
+
+        stamina -= staminaToConsume;
+        onStaminaChange?.Invoke(stamina, maxStamina);
+        return true;
+    }
+
+    public bool HandlePurchase(Object newPurchase)
+    {
+        Ability itemAbility = newPurchase as Ability;
+        if (itemAbility == null) return false;
+
+        //GiveAbility(itemAbility);
+
+        return true;
+    }
+}
---
+++ SurvivalLegends/Assets/AbilitySystem/AbilityDock2.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AbilityDock2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] AbilityComponent abilityComponent;
+    [SerializeField] RectTransform Root;
+    [SerializeField] VerticalLayoutGroup LayoutGrp;
+    [SerializeField] AbilityUI AbilityUIPrefab;
+
+    [SerializeField] float ScaleRange = 200f;
+
+    [SerializeField] float highLightSize = 1.5f;
+    [SerializeField] float ScaleSpeed = 20f;
+
+    Vector3 GoalScale = Vector3.one;
+
+    List<AbilityUI> abilityUIs= new List<AbilityUI>();
+
+    PointerEventData touchData;
+    AbilityUI hightLightedAbility;
+    private void Awake()
+    {
+        abilityComponent.onNewAbilityAdded += AddAbility;
+    }
+
+    private void AddAbility(Ability newAbility)
+    {
+        AbilityUI newAbilityUI = Instantiate(AbilityUIPrefab, Root);
+        newAbilityUI.Init(newAbility);
+        abilityUIs.Add(newAbilityUI);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(touchData !=null)
+        {
+            GetUIUnderPointer(touchData, out hightLightedAbility);
+            ArrangeScale(touchData);
+        }
+
+            transform.localScale = Vector3.Lerp(transform.localScale, GoalScale, Time.deltaTime * ScaleSpeed);
+    }
+
+    private void ArrangeScale(PointerEventData touchData)
+    {
+        if (ScaleRange == 0) return;
+
+
+
+        float touchVerticalPos = touchData.position.y;
+        foreach(AbilityUI abilityUI in abilityUIs)
+        {
+            float abilityUIVerticalPos = abilityUI.transform.position.y;
+            float distance = Mathf.Abs(touchVerticalPos - abilityUIVerticalPos);
+
+            if(distance > ScaleRange)
+            {
+                abilityUI.SetScaleAmt(0);
+                continue;
+            }
+
+            float scaleAmt = (ScaleRange - distance) / ScaleRange;
+            abilityUI.SetScaleAmt(scaleAmt);
+
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        touchData = eventData;
+        GoalScale = Vector3.one * highLightSize;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (hightLightedAbility)
+        {
+            hightLightedAbility.ActivateAbility();
+        }
+        touchData = null;
+        ResetScale();
+        GoalScale = Vector3.one;
+    }
+
+    private void ResetScale()
+    {
+        foreach(AbilityUI abilityUI in abilityUIs)
+        {
+            abilityUI.SetScaleAmt(0);
+        }
+    }
+
+    bool GetUIUnderPointer(PointerEventData eventData, out AbilityUI abilityUI)
+    {
+        List<RaycastResult> findAbility =new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, findAbility);
+
+        abilityUI = null;
+        foreach(RaycastResult result in findAbility) 
+        {
+            abilityUI = result.gameObject.GetComponentInParent<AbilityUI>();
+            if(abilityUI != null)
+                return true;
+        }
+        return false;
+
+    }
+}
