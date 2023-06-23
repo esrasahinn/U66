fb96c28564cd97ec3351dbfe999ac37b0a1f344e
Serclarus <serefsakaci@hotmail.fr>
2023-06-23 21:22:26 +0300

    LevelSelector temeli atıldı

---
+++ SurvivalLegends/Assets/Scripts/Seref/LevelMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+    public void OpenMap(int mapId)
+    {
+        string MapName = "Map " + mapId;
+        SceneManager.LoadScene(MapName);
+    }
+
+}
