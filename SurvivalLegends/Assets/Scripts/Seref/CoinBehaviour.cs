df1e28f206c0d1bd1f2cf6915823992147eaadad
Serclarus <serefsakaci@hotmail.fr>
2023-06-27 22:36:21 +0300

    coin sondaki hızı arttırıldı

--- SurvivalLegends/Assets/Scripts/Seref/CoinBehaviour.cs
+++ SurvivalLegends/Assets/Scripts/Seref/CoinBehaviour.cs
@@ -16,7 +16,6 @@ public class CoinBehaviour : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        // animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -24,9 +23,7 @@ public class CoinBehaviour : MonoBehaviour
         if (transform.position.y <= stopY)
         {
             rb.isKinematic = true;
-            //transform.position = new Vector3(transform.position.x, stopY, transform.position.z);
             StartCoroutine(FlyToPlayer());
-            // animator.SetBool("landed", true);
         }
     }
 
@@ -37,19 +34,34 @@ public class CoinBehaviour : MonoBehaviour
 
     IEnumerator FlyToPlayer()
     {
-        yield return new WaitForSeconds(1f);
-        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position, ref velocity, Time.deltaTime * Random.Range(minFolSpeed, maxFolSpeed));
-        //// Debug.Log("FlyToPlayer coroutine started.");
-        // yield return new WaitForSeconds(1f);
+        float smoothTime = Time.deltaTime * Random.Range(minFolSpeed, maxFolSpeed);
+        float maxDistance = Vector3.Distance(transform.position, target.position);
+        float thresholdDistance = 2f; // Adjust this threshold to control when to start decreasing smoothTime
 
-        // Vector3 targetPosition = target.position + new Vector3(0, 1, 0);
-        // while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
-        // {
-        //     transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, Time.deltaTime * Random.Range(minFolSpeed, maxFolSpeed));
-        //    // Debug.Log("Current position: " + transform.position);
-        //    // Debug.Log("Target position: " + targetPosition);
-        //     yield return null;
-        // }
-        // //Debug.Log("Coin reached the player.");
+        while (transform.position != target.position)
+        {
+            if (Vector3.Distance(transform.position, target.position) <= thresholdDistance)
+            {
+                smoothTime = 0.05f;
+            }
+
+            transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+            yield return null;
+        }
     }
 }
+
+//// Debug.Log("FlyToPlayer coroutine started.");
+// yield return new WaitForSeconds(1f);
+
+// Vector3 targetPosition = target.position + new Vector3(0, 1, 0);
+// while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+// {
+//     transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, Time.deltaTime * Random.Range(minFolSpeed, maxFolSpeed));
+//    // Debug.Log("Current position: " + transform.position);
+//    // Debug.Log("Target position: " + targetPosition);
+//     yield return null;
+// }
+// //Debug.Log("Coin reached the player.");
+//  }
+//}
