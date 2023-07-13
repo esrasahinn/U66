914702d993a11e8bc8d9c23e0d78bee282a2d290
minjishii <fulyaates11@hotmail.com>
2023-07-13 22:17:44 +0300

    guncellbi kaç kod

--- SurvivalLegends/Assets/Scripts/Furkan/RifleFire.cs
+++ SurvivalLegends/Assets/Scripts/Furkan/RifleFire.cs
@@ -16,6 +16,6 @@ public class RifleFire : MonoBehaviour
     private void Aktiflestir()
     {
         rifleScript.AktiflestirOzelYetenek();
-        // audiosource.Play();
+         audiosource.Play();
     }
 }
