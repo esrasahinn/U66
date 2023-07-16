211d423a754352f56ef886edec3ded409136df45
mahmut <msrky1@gmail.com>
2023-07-17 01:15:08 +0300

    Sahne Düzemleme

--- SurvivalLegends/Assets/Scripts/Mahmut/AutoSave/PlayerScripts.cs
+++ SurvivalLegends/Assets/Scripts/Mahmut/AutoSave/PlayerScripts.cs
@@ -12,43 +12,56 @@ public class PlayerScripts : MonoBehaviour
     public int XP;
     public TMP_Text levelText;
     public TMP_Text levelText2;
-  
+
 
 
     private void Start()
     {
-         
+
         LoadPlayer();
     }
 
-  public void SavePlayer()
-{
+    public void SavePlayer()
+    {
 
 
-    level++;
+        level++;
 
-    // Diğer kaydetme işlemleri
-    SaveSystem.SavePlayer(this);
-    SceneManager.LoadScene(8);
-    Debug.Log("Kayıt Başarılı!");
-    Debug.Log(SaveSystem.LoadPlayer());
-}
+        // Diğer kaydetme işlemleri
+        SaveSystem.SavePlayer(this);
+        SceneManager.LoadScene(8);
+        Debug.Log("Kayıt Başarılı!");
+        Debug.Log(SaveSystem.LoadPlayer());
+    }
 
 
 
     public void LoadPlayer()
     {
         PlayerData data = SaveSystem.LoadPlayer();
-        
-       
+
+
         level = data.level;
         health = data.health;
         XP = data.XP;
-         levelText.text = level.ToString();
+        levelText.text = level.ToString();
         levelText2.text = level.ToString();
         Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
         transform.position = position;
 
         Debug.Log("Geri Yükleme Başarılı!");
     }
+
+
+    public void RestartGame()
+    {
+
+
+        SceneManager.LoadScene("LevelSelector");
+
+
+
+    }
+
+
 }
